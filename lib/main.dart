e8b97b7d30e8004e8c2cac3d90f4e09dab43d48c
khandaker-shamsul-arafen <khandakershamsul@gmail.com>
2023-05-16 17:14:32 +0600

    Project Structur Build

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 import 'app/routes/app_pages.dart';
@@ -13,7 +14,23 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return
+    return ScreenUtilInit(
+      designSize: const Size(412, 915),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return GetMaterialApp(
+          initialRoute: AppPages.INITIAL,
+          getPages: AppPages.routes,
+          title: 'Medicine Alarm App',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+        );
+      },
+    );
   }
 }
 
